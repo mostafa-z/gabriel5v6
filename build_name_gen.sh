4041d3802fef9ec780cf790e18595d8e4056ec16
mostafa-z <mostafazarghami@gmail.com>
2015-06-03 02:02:53 +0430

    fix build script

--- build_name_gen.sh
+++ build_name_gen.sh
@@ -2,4 +2,4 @@
 
 today=$(date '+%Y_%m_%d__%H_%M_%S');
 
-sed -i '3s/.*/CONFIG_LOCALVERSION="'gabriel-nightly-"$today"'"/' /arch/arm/configs/g3-*
+sed -i '3s/.*/CONFIG_LOCALVERSION="'gabriel-nightly-"$today"'"/' arch/arm/configs/g3-*
